a8b250361918dcea586c51cda3403db6dc753ba6
Hwang Heejung <76714312+hwangheejung@users.noreply.github.com>
2024-08-21 20:25:36 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -0,0 +1,14 @@
+function solution(s) {
+    var answer = '';
+    const arr = s.split(" ").map(x=>+x)
+    let max =arr[0];
+    let min = arr[0];
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]>max)
+            max=arr[i];
+        if(arr[i]<min)
+            min=arr[i];
+    }
+    answer = `${min} ${max}`
+    return answer;
+}
